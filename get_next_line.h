9f12a3db28bb8f98b903cf8bcd98664675eb91b6
Lanila <109435712+Pakegen@users.noreply.github.com>
2024-07-18 14:59:58 +0200

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: quenalla <quenalla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 15:56:51 by quenalla          #+#    #+#             */
+/*   Updated: 2024/07/18 14:32:38 by quenalla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include<unistd.h>
+# include<stdio.h>
+# include<fcntl.h>
+# include<stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# ifndef LIMIT_FD
+#  define LIMIT_FD 1024
+# endif
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *str);
+
+char	*ft_strchr(const char *s, int c);
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_substr(char const *s, unsigned start, size_t len);
+
+char	*ft_strdup(const char *s);
+
+#endif
